222b2a60b66def290162bf2c52a0cef5c29b83a5
majid <majid@gmail.com>
2019-01-07 22:19:54 +0330

    add try again to letter_geuss

--- letter_geuss.py
+++ letter_geuss.py
@@ -2,7 +2,7 @@
 from random import randrange,randint
 import os
 
-words=('banana','student','teacher','python','canon','watch')
+words=('banana','student','teacher','python','Orange','Lemon')
 
 
 
@@ -10,18 +10,10 @@ random_index=randint(0,len(words)-1)
 selected_word=words[random_index]
 
 current_life=0
-max_life=6
-input_list=[]
-guess_word='_'*len(selected_word)
+max_life=5
+input_list = []
+guess_word = '-'*len(selected_word)
 
-
-def again():
-    random_index=randint(0,len(words)-1)
-    selected_word=words[random_index]
-    print(selected_word)
-    current_life=0
-    input_list=[]
-    guess_word='_'*len(selected_word)
  
 
 while True:
@@ -30,28 +22,28 @@ while True:
     print()
     print()
     print(' '.join(input_list))
+    print()
     print(guess_word)
+    print()
     input_letter=input('Guess a letter: ')
     if input_letter in selected_word:
         for i in range(len(selected_word)):
             if (selected_word[i] == input_letter):
                 list_guess_word=list(guess_word)
                 list_guess_word[i]=selected_word[i]
-                guess_word=''.join(list_guess_word) 
-        if '_' not in guess_word:
-            print('You Win.')
-            
+                guess_word=''.join(list_guess_word)
+                print(guess_word) 
+        if '-' not in guess_word:
+            print('***************************')
+            print('         You Win.')
+            print('***************************')
             playagain=input('Do you want play again? [Y/N] ')
-            if playagain == 'y':
-                again()
-        #        #os.system('cls') if os.name=='nt' else os.system('clear')
-        #        random_index=randint(0,len(words)-1)
-        #        selected_word=words[random_index]
-        #        print(selected_word)
-        #        current_life=0
-        #        input_list=[]
-     #        guess_word='_'*len(selected_word)
-                continue
+            if playagain == 'y' or playagain=='Y':
+                random_index=randint(0,len(words)-1)
+                selected_word=words[random_index]
+                current_life=0
+                input_list=[]
+                guess_word='-'*len(selected_word)
             else:
                 break
     else:
@@ -60,8 +52,19 @@ while True:
 
 
     if current_life > max_life:
-        print('you lose!!')
-        break
+        print('***************************')
+        print('         you lose!!')
+        print('***************************')
+        playagain=input('Do you want play again? [Y/N] ')
+        if playagain == 'y' or playagain=='Y':
+            random_index=randint(0,len(words)-1)
+            selected_word=words[random_index]
+            print(selected_word)
+            current_life=0
+            input_list=[]
+            guess_word='-'*len(selected_word)
+        else:
+            break        
 
  
 
